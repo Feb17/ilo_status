8e79dec9f53d3d088ee59c2b40d164be66fee478
agu2szh <aaron.gu@cn.bosch.com>
2023-03-08 19:03:32 +0800

    update

--- ilo_status/urls.py
+++ ilo_status/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views as index_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('__debug__/', include('debug_toolbar.urls')),
     path('', index_views.index, name='index'),
     path('test/', index_views.test, name='test'),
 ]
--- ilo_status/views.py
+++ ilo_status/views.py
@@ -1,9 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect, resolve_url
 
 
 def index(request):
     return render(request, 'index.html')
 
+
 def test(request):
-    return render(request, 'test.html')
+    if request.method == 'POST':
+        # 从请求数据中获取server_name的值
+        server_name = request.POST.get('server_name')
+        print(server_name)
+
+        # 返回一个JSON响应，告诉客户端请求成功，并提供一些信息
+        data = {'status': 'success', 'message': 'The server name is ' + server_name}
+        return JsonResponse(data)
+
+    # 如果请求方法不是POST，则返回一个空白的响应
+    return JsonResponse({})
+
