1b52135bed001cba0860de7417f3eaf25c4813c3
agu2szh <aaron.gu@cn.bosch.com>
2023-03-09 18:39:19 +0800

    update

--- ilo_status/views.py
+++ ilo_status/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect, resolve_url
-from ilo_status.script import get_ilo_status
+from .script import get_ilo_status
 
 
 def index(request):
@@ -12,7 +12,7 @@ def test(request):
         # 从请求数据中获取server_name的值
         server_ip = request.POST.get('server_ip')
 
-        host_status_data = get_ilo_status.get_ilo_status(server_ip)
+        host_status_data = get_ilo_status.get_ilo_status('https://' + server_ip)
 
         if host_status_data:
             # 返回一个JSON响应，告诉客户端请求成功，并提供一些信息
